e41cd1e7f4b14ec17a7acbec93c07012ba752bdb
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2025-06-03 00:28:21 +0000

    Completed resizable navbar with iframe

--- local-library/src/App.jsx
+++ local-library/src/App.jsx
@@ -1,18 +1,31 @@
+import React from 'react';
+import {
+  PanelGroup,
+  Panel,
+  PanelResizeHandle,
+} from 'react-resizable-panels';
 import './App.css';
 
 function App() {
   return (
     <div className="container">
-      <nav className="sidebar">
-        <ul>
-          <li><a href="/category1.html" target="content-frame">Category I</a></li>
-          <li><a href="/category2.html" target="content-frame">Category II</a></li>
-          <li><a href="/category3.html" target="content-frame">Category III</a></li>
-        </ul>
-      </nav>
-      <main className="content">
-        <iframe name="content-frame" title="Content Frame" className="iframe" />
-      </main>
+      <PanelGroup direction="horizontal">
+        <Panel defaultSize={20} minSize={15} maxSize={40}>
+          <div className="sidebar">
+            <ul>
+              <li><a href="/category1.html" target="content-frame">Category I</a></li>
+              <li><a href="/category2.html" target="content-frame">Category II</a></li>
+              <li><a href="/category3.html" target="content-frame">Category III</a></li>
+            </ul>
+          </div>
+        </Panel>
+        <PanelResizeHandle className="resizer" />
+        <Panel>
+          <div className="content">
+            <iframe name="content-frame" title="Content Frame" className="iframe" />
+          </div>
+        </Panel>
+      </PanelGroup>
     </div>
   );
 }
